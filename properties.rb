be8eb9b87555611f1ac211aa3e2f7b7273b383a0
Niclas Schirrmeister <nschirrmeister@web.de>
2020-02-03 22:33:59 +0100

    add schedule era a

--- properties.rb
+++ properties.rb
@@ -1,6 +1,6 @@
 def properties
     {
-        texture_resolution: '512x512',
+        texture_resolution: '1024x1024',
         author_prefix: 'eisfeuer',
         major_version: 1,
         technical_name: 'modular_tram_station'
