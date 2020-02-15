2e9c8f7112e15b0ebd6d0d3eb2fa19fae506cae2
Niclas Schirrmeister <nschirrmeister@web.de>
2020-02-15 23:01:38 +0100

    add textures for catenary models

--- properties.rb
+++ properties.rb
@@ -1,6 +1,6 @@
 def properties
     {
-        texture_resolution: '1024x1024',
+        texture_resolution: '512x512',
         author_prefix: 'eisfeuer',
         major_version: 1,
         technical_name: 'modular_tram_station'
