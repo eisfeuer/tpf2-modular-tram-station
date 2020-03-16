79575bdf41c0d4f9439647ec7b9b9bc5639aaec3
Niclas Schirrmeister <nschirrmeister@web.de>
2020-03-17 00:12:28 +0100

    add strings lua

---
+++ mod/strings.lua
@@ -0,0 +1,54 @@
+function data()
+    return {
+      en = {
+        ["modular_tram_station_name"] = "modular_tram_station_name",
+        ["modular_tram_station_desc"] = "modular_tram_station_desc",
+        -- Blueprint settings keys
+        ["modutram_platform_placing_pattern"] = "modutram_platform_placing_pattern",
+        ["modutram_tram_tracks"] = "modutram_tram_tracks",
+        ["modutram_electrification"] = "modutram_electrification",
+        ["modutram_bus"] = "modutram_bus",
+        ["modutram_street_connections"] = "modutram_street_connections",
+        ["modutram_platforms_left"] = "modutram_platforms_left",
+        ["modutram_platforms_right"] = "modutram_platforms_right",
+        ["modutram_platform_length"] = "modutram_platform_length",
+        ["modutram_platform_passenger_access"] = "modutram_platform_passenger_access",
+        ["modutram_shelter"] = "modutram_shelter",
+        ["modutram_shelter_size"] = "modutram_shelter_size",
+        ["modutram_fences"] = "modutram_fences",
+        ["modutram_lamps"] = "modutram_lamps",
+        ["modutram_benches"] = "modutram_benches",
+        ["modutram_billboards"] = "modutram_billboards",
+        ["modutram_stop_signs"] = "modutram_stop_signs",
+        ["modutram_station_signs"] = "modutram_station_signs",
+        ["modutram_destination_display"] = "modutram_destination_display",
+        -- Blueprint settings options
+        ["era_a"] = "Era A",
+        ["era_b"] = "Era B",
+        ["era_c"] = "Era C",
+        ["left"] = "Left",
+        ["right"] = "Right",
+        ["both"] = "Both",
+        ["small"] = "Small",
+        ["mid"] = "Medium",
+        ["large"] = "Large",
+        -- Modules general descriptions
+        ["tram_exit_right"] = "tram_exit_right",
+        -- Modules
+        ["module_bus_lane_name"] = "module_bus_lane_name",
+        ["module_bus_lane_desc"] = "module_bus_lane_desc",
+        ["module_platform_access_name"] = "module_platform_access_desc",
+        ["module_platform_single_left_name"] = "module_platform_single_left_name",
+        ["module_platform_single_right_name"] = "module_platform_single_right_name",
+        ["module_street_connection_double_name"] = "module_street_connection_double_name",
+        ["module_street_connection_in_name"] = "module_street_connection_in_name",
+        ["module_street_connection_out_name"] = "module_street_connection_out_name",
+        ["module_street_connection_in_name"] = "module_street_connection_in_name",
+        ["module_track_double_name"] = "module_street_connection_in_name",
+        ["module_track_down_name"] = "module_track_down_name",
+        ["module_track_up_name"] = "module_track_up_name",
+        ["module_track_desc"] = "module_track_desc"
+      },
+    }
+end
+    
